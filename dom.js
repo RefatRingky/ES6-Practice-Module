2834bee0e6b8003b1d265dcbe63afb680ad5c051
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-24 01:09:00 +0600

    first commit

---
+++ dom.js
@@ -0,0 +1,4 @@
+document.getElementById('add-border').addEventListener('click',function () {
+    const friends = document.getElementById('Friend-container');
+    friends.style.border = '2px solid orange';
+})
