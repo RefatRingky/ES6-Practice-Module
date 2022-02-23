9eb72767543143e61d5455455bb3b74ae5236431
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-24 01:34:49 +0600

    second commit

--- dom.js
+++ dom.js
@@ -2,3 +2,21 @@ document.getElementById('add-border').addEventListener('click',function () {
     const friends = document.getElementById('Friend-container');
     friends.style.border = '2px solid orange';
 })
+
+// Add the Background Color
+function addBackground(){
+    const Friends = document.getElementsByClassName("friend");
+    for(const friend of Friends){
+        friend.style.backgroundColor = "orange";
+    }
+}
+// add friend
+document.getElementById('add-friend').addEventListener('click',function () {
+    const container =document.getElementById('Friend-container');
+    const friendDiv = document.createElement('div');
+    friendDiv.classList.add('friend') ;
+    friendDiv.innerHTML = `
+    <h3 class="friend-name">Friend-1</h3>
+    <p>Lorem, ipsum.</p>`
+    container.appendChild(friendDiv);
+})
