e2b377bc39598596fdfe51338370e743c55793ee
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-24 02:59:20 +0600

    third commit

---
+++ arrow.js
@@ -0,0 +1,24 @@
+// function declaration
+function add1(num1,num2) {
+    return num1 +num2;
+}
+// function declartion using varaible
+
+const add2 = function add2(num1,num2) {
+    return num1 + num2;
+}
+// anonymous Function
+const add3 = function(num1,num2) {
+    return num1 + num2;
+}
+// arrow function
+const add4 = (num1,num2) => num1+num2;
+
+const sum = add1(10,20);
+console.log(sum);
+const sum2 = add2(10,20);
+console.log(sum2);
+const sum3 = add3(10,20);
+console.log(sum3);
+const sum4 = add4(10,20);
+console.log(sum4);
